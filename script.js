d6c6ffeb11e29a7dcdb05304fc17d9937e08e671
Nigel Kibodeaux <nigelmail@gmail.com>
2014-02-07 00:39:27 -0900

    Pretty much playable now

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ window.canvasWidth = canvasElement.width;
 window.canvasHeight = canvasElement.height;
 window.buildingSpacing = 250;
 window.buildingWidth = 100;
+window.ballRadius = 15;
 window.buildings = [];
+window.pointsCounter = document.getElementById('points');
+
 
 function startStop(){  
 	if(on) {
@@ -24,6 +27,9 @@ function flap() {
 		// modify ball movement
 		Vy = Vy - powerFactor;
 	}
+	else {
+		resetGame();
+	}
 }
 
 function lose() {
@@ -31,9 +37,14 @@ function lose() {
 }
 
 function Building(offset) {
-	this.gap = 300;
-	this.centerline = 400;
-	this.x = canvasElement.width + offset;
+	function randomIntFromInterval(min,max) {
+    	return Math.floor(Math.random()*(max-min+1)+min);
+	}
+	
+	this.gap = randomIntFromInterval(150, 300);
+	this.centerline = randomIntFromInterval(150, canvasHeight - 150);
+	this.x = offset;
+	this.conquered = false;
 }
 Building.prototype.isOverlap = function(x, y, radius) {
 	// both thingies
@@ -52,6 +63,11 @@ Building.prototype.isOverlap = function(x, y, radius) {
 
 		if (y+radius > minYBottom) // bottom
 			lose();
+	}
+	else if (x-radius > maxX) {
+		if (!this.conquered)
+			pointsCounter.value = parseInt(pointsCounter.value) + 1;
+		this.conquered = true;
 	}
 }
 
@@ -76,7 +92,7 @@ function draw() {
 	
 	// draw buildings
 	buildings.forEach(function(b){
-		b.x--
+		b.x = b.x - scrollSpeed;
 		ctx.fillRect(b.x, b.centerline + b.gap/2, buildingWidth, canvasHeight - b.centerline - b.gap/2);
 		ctx.fillRect(b.x, 0, buildingWidth, b.centerline - b.gap/2);
 		
@@ -91,7 +107,7 @@ function draw() {
 		buildings.shift();
 		
 		// add a new one
-		var offset = 0; //buildings[buildings.length-1].x + buildingSpacing + buildingWidth;
+		var offset = buildings[buildings.length-1].x + buildingSpacing;
 		buildings.push(new Building(offset));
 	}
 
@@ -113,23 +129,19 @@ function draw() {
 function resetGame() {
 	 
 	speedMultiplier = 1;
-	x = 400;
-	y = 300;
+	x = canvasWidth / 2;
+	y = canvasHeight / 2;
 	Vy = 0;
-	Ax = 0;
-	Ay = .0015 * speedMultiplier;
+	Ay = .0015;
 	//Ay = 0;
 	scrollSpeed = 1 * speedMultiplier;
 	powerFactor = 1 * speedMultiplier;
-	ballRadius = 15;
 	lastTime = new Date().getTime();
 	
 	buildings = [
-		new Building(0 * window.buildingSpacing + window.buildingWidth),
-		new Building(1 * window.buildingSpacing + window.buildingWidth),
-		new Building(2 * window.buildingSpacing + window.buildingWidth)/*,
-		new Building(3 * window.buildingSpacing + window.buildingWidth),
-		new Building(4 * window.buildingSpacing + window.buildingWidth)*/
+		new Building(canvasElement.width + 0 * window.buildingSpacing + window.buildingWidth),
+		new Building(canvasElement.width + 1 * window.buildingSpacing + window.buildingWidth),
+		new Building(canvasElement.width + 2 * window.buildingSpacing + window.buildingWidth)
 	];
 
 	startStop();//runLoop = setInterval(draw, 10);
@@ -142,8 +154,12 @@ window.addEventListener('load', function() {
 	//canvasElement.addEventListener('mouseover', function(){if(!on)startStop();}, false);
 	document.addEventListener('keydown', flap, false);
 	//xDisplay = document.getElementById('x');
-	//yDisplay = document.getElementById('y');
+	//yDisplay = document.getElementById('y');	
 	
-	resetGame();
-
+	// draw ball
+	ctx.beginPath();
+	ctx.arc(canvasWidth/2, canvasHeight/2, ballRadius, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.fill();
+	
 }, false);
